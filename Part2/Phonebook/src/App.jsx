bac115af6f8a626eb6147305af4de552ee99b3f1
Gracmit <aleksii97@gmail.com>
2025-03-12 14:46:43 +0200

    Exercise 2.8

--- Part2/Phonebook/src/App.jsx
+++ Part2/Phonebook/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: "04040404040"
+     }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault();
     const newPerson = {
       name: newName,
+      number: newNumber
     }
 
     var sameName = persons.find((person) => person.name === newPerson.name);
@@ -27,6 +31,10 @@ const App = () => {
     setNewName(event.target.value);
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -35,13 +43,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {
         persons.map(person => 
-          <p key={person.name}>{person.name}</p>)
+          <p key={person.name}>{person.name} {person.number}</p>)
       }
     </div>
   )
