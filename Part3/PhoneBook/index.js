594b6242f61cf25ec801bbbb6650d74fee6aa03b
Gracmit <aleksii97@gmail.com>
2025-04-15 12:55:26 +0300

    Exercise 3.2

--- Part3/PhoneBook/index.js
+++ Part3/PhoneBook/index.js
@@ -2,34 +2,36 @@ const express = require('express')
 const app = express()
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
-// app.get('/', (request, response) => {
-//   response.send('<h1>Hello World!</h1>')
-// })
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>` + 
+        `<p>${new Date().toString()}</p>`)
+})
 
 app.get('/api/persons', (request, response) => {
-    if(persons){
+    if (persons) {
         response.json(persons)
     } else {
         response.status(404).end()
@@ -38,5 +40,5 @@ app.get('/api/persons', (request, response) => {
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
