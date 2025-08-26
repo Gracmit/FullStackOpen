bb3a25bb1fe6dfeb5708e7b6ee9a817d7d35dc51
Gracmit <aleksii97@gmail.com>
2025-08-26 15:21:51 +0300

    Exercise 4.9

--- Part4/tests/blog_api.test.js
+++ Part4/tests/blog_api.test.js
@@ -9,16 +9,21 @@ const Helper = require('./test_helper')
 const api = supertest(app)
 
 beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(Helper.initialBlogs)
+  await Blog.deleteMany({})
+  await Blog.insertMany(Helper.initialBlogs)
 })
 
-test('Get all Blogs', async () =>{
-    const response = await api.get('/api/blogs')
+test('Get all Blogs', async () => {
+  const response = await api.get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(response.body.length, Helper.initialBlogs.length)
+  assert.strictEqual(response.body.length, Helper.initialBlogs.length)
+})
+
+test('Is id written correctly', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(Object.keys(response.body[0]).find(key => key === 'id'), 'id')
 })
 
 
