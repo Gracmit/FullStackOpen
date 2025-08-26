b0cf8b58758ac4d1335b9b2bc99d849d2ee77234
Gracmit <aleksii97@gmail.com>
2025-08-26 15:28:51 +0300

    Exercise 4.10

--- Part4/tests/blog_api.test.js
+++ Part4/tests/blog_api.test.js
@@ -26,6 +26,30 @@ test('Is id written correctly', async () => {
   assert.strictEqual(Object.keys(response.body[0]).find(key => key === 'id'), 'id')
 })
 
+test('Blog gets added to database', async () => {
+  const blog =
+  {
+    title: 'First class tests 2',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions2.htmll',
+    likes: 102,
+  }
+
+
+  await api
+    .post('/api/blogs')
+    .send(blog)
+    .expect(201)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, Helper.initialBlogs.length + 1)
+
+  const contents = response.body.map(r => r.url)
+
+  assert(contents.includes('http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions2.htmll'))
+})
+
 
 after(async () => {
   await mongoose.connection.close()
